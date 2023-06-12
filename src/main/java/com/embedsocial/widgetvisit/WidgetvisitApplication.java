ebb33653ba24bceda1cb022bef0919b34e448344
Filip Koteski <fkote@Filips-MacBook-Pro-2.local>
2023-06-12 21:26:48 +0200

    java init

---
+++ src/main/java/com/embedsocial/widgetvisit/WidgetvisitApplication.java
@@ -0,0 +1,13 @@
+package com.embedsocial.widgetvisit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WidgetvisitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WidgetvisitApplication.class, args);
+	}
+
+}
